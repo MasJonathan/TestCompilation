9532b16d6363a56b83243d159c04c2a125c1be9c
JonathanMAS <jonathanmas.public@gmail.com>
2018-04-05 16:31:21 +0200

    some changes

--- Builds/VisualStudio2017/tetst.bat
+++ Builds/VisualStudio2017/tetst.bat
@@ -1,9 +1,9 @@
-call git-bash sed -i -e 's/#define OC_VERSION "1.0.0"/#define OC_VERSION "1.0.1"' afile.txt
+:sed -i -e 's/#define OC_VERSION "1.0.0"/#define OC_VERSION "1.0.1"' afile.txt
 
 :while read a ; do echo ${a//#define OC_VERSION "1.0.0"/#define OC_VERSION "1.0.1"} ; done < /afile.txt > /afile.txt.t ; mv /afile.txt{.t,}
 
-:file_contents=$(<./afile.txt)
-:src=$'#define OC_VERSION "1.0.0"'
-:dest=$'#define OC_VERSION "1.0.1"'
-:echo "${file_contents//src/dest}" > afile.txt
+file_contents=$(<./afile.txt)
+src='#define OC_VERSION "1.0.0"'
+dest='#define OC_VERSION "1.0.1"'
+echo "${file_contents//src/dest}" > afile.txt
 pause
