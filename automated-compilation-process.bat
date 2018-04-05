99bbedeaac493a8d69cc19aab6e09cd4f72947c0
JonathanMAS <jonathanmas.public@gmail.com>
2018-04-05 16:35:49 +0200

    update

--- automated-compilation-process.bat
+++ automated-compilation-process.bat
@@ -1,6 +1,8 @@
-projucer.exe --get-version TestCompilation.jucer 
-projucer.exe --set-version 0.1.0 TestCompilation.jucer 
+git-bash rebase
+:projucer.exe --get-version TestCompilation.jucer 
+:projucer.exe --set-version 1.0.0 TestCompilation.jucer 
 projucer.exe --resave TestCompilation.jucer 
 devenv.exe Builds/VisualStudio2017/TestCompilation.sln /Clean Release
 devenv.exe Builds/VisualStudio2017/TestCompilation.sln /Build Release
+
 pause
